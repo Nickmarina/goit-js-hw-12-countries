11ad0f38bd099f476c0022e7b0cf8f990a38a198
Marina Permiakova <marinapermyakova13@gmail.com>
2021-02-21 14:39:52 +0200

    removes unnecessary from fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,6 @@ function fetchCountries(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
   return fetch(url)
     .then(res => res.json())
-    .then(countries => countries)
     .catch(error => console.log(error));
 }
 export default fetchCountries;
